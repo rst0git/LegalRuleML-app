06f90e30c2712320004c43cf3490dc80c03e3f19
Radostin Stoyanov <rstoyanov1@gmail.com>
2017-07-10 08:31:39 +0100

    auth: Disable registration
    Require authentication to register new user.

--- src/resources/views/inc/navbar.blade.php
+++ src/resources/views/inc/navbar.blade.php
@@ -28,11 +28,12 @@
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ route('login') }}">Login</a></li>
-                    <li><a href="{{ route('register') }}">Register</a></li>
                 @else
                     <li>
                       <a href="/doc/upload">Upload Document</a>
                     </li>
+                    <li><a href="{{ route('register') }}">Create account</a></li>
+
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
