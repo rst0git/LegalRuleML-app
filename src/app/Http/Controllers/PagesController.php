8c03ea41a0e774846442329ca99d278467df385e
Andrea Faulds <ajf@ajf.me>
2017-07-17 14:21:51 +0100

    Redirect / to /dashboard if logged in

--- src/app/Http/Controllers/PagesController.php
+++ src/app/Http/Controllers/PagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 // PagesController is used only for the Home Page
 class PagesController extends Controller
@@ -17,6 +18,9 @@ class PagesController extends Controller
     */
 
     public function index() {
+        if (Auth::check()) {
+            return redirect(route('dashboard'));
+        }
         return view('index');
     }
 }
