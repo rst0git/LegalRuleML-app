519b3b9d21041a2c28a20bd3c17084a356154fc5
Radostin Stoyanov <rstoyanov1@gmail.com>
2017-09-14 16:22:40 +0100

    Explicitly set upload action

--- src/resources/views/documents/upload.blade.php
+++ src/resources/views/documents/upload.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <div class="container" style="width:400px;">
-        {!! Form::open(array('method'=>'POST', 'files'=>true)) !!}
+        {!! Form::open(array('method'=>'POST', 'action' => 'DocumentsController@store', 'files'=>true)) !!}
         <div class="form-group">
             {{ Form::file('files[]', array('multiple'=>true)) }}
         </div>
